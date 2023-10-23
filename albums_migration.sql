ec1be35b7351d7805a77aa6b5cf7bf3625c46bde
Gerald Walker <gerald.e.walker05@gmail.com>
2023-10-23 14:53:00 -0500

    working

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,10 +6,10 @@ drop table if exists albums;
 CREATE TABLE albums
     (
         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-        artist VARCHAR(50) NOT NULL,
-        name VARCHAR(50) NOT NULL,
+        artist_name VARCHAR(50) NOT NULL,
+        album_name VARCHAR(50) NOT NULL,
         release_date int (4) NOT NULL,
-        sales DECIMAL(4,1) NOT NULL,
         genre VARCHAR(50) NOT NULL,
+        sales DECIMAL(4,1) NOT NULL,
         PRIMARY KEY (id)
     );
