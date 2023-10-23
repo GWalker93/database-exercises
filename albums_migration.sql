8e674759d94538a727e22822bb87c09b2804ad33
Gerald Walker <gerald.e.walker05@gmail.com>
2023-10-23 12:23:48 -0500

    todays work

---
+++ albums_migration.sql
@@ -0,0 +1,15 @@
+
+use codeup_test_db;
+
+drop table if exists albums;
+
+CREATE TABLE albums
+    (
+        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+        artist VARCHAR(50) NOT NULL,
+        name VARCHAR(50) NOT NULL,
+        release_date int (4) NOT NULL,
+        sales DECIMAL(4,1) NOT NULL,
+        genre VARCHAR(50) NOT NULL,
+        PRIMARY KEY (id)
+    );
