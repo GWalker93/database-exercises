2a40255aa7c0ac9b00911a63f70f6e81a62effc7
Gerald Walker <gerald.e.walker05@gmail.com>
2023-10-26 16:40:13 -0500

    Done

--- albums_migration.sql
+++ albums_migration.sql
@@ -13,3 +13,41 @@ CREATE TABLE albums
         sales DECIMAL(6,1) NOT NULL,
         PRIMARY KEY (id)
     );
+
+ALTER TABLE albums
+    ADD UNIQUE (album_name, artist_name);
+
+-- Although not explicitly covered in the lesson, aggregate functions like count can be used with join queries. Use COUNT and the appropriate join type to get a list of roles along with the number of users that have a given role. Hint: You will also need to use GROUP BY in the query.
+
+SELECT COUNT(*) as count
+FROM roles
+GROUP BY id
+;
+
+-- mysql> SELECT users.name as name, roles.name as role FROM users RIGHT JOIN roles ON users.role_id = roles.id;
+
+SELECT distinct roles.name as role FROM roles RIGHT JOIN users ON users.role_id = roles.id;
+
+SELECT distinct roles.name as role, count(*) as count
+FROM roles
+RIGHT JOIN users ON users.role_id = roles.id
+group by role;
+
+SELECT distinct roles.name as role, count(*) as count
+FROM users
+Right JOIN roles ON users.role_id = roles.id
+GROUP BY id
+;
+
+
+SELECT users.name as name, roles.name as role
+FROM users
+RIGHT JOIN roles ON users.role_id = roles.id;
+
+SELECT distinct roles.name as role, count(*) as count
+FROM roles
+    RIGHT JOIN users ON users.role_id = roles.id group by role;
+
+SELECT roles.name as role, count(*) as count
+FROM roles
+JOIN users ON users.role_id = roles.id group by role;
---
+++ join_exercises.sql
@@ -0,0 +1,44 @@
+use employees;
+
+select d.dept_name as Department_Name, concat(e.first_name, ' ', e.last_name ) as Department_Manager
+from employees as e
+join dept_manager as dm
+on dm.emp_no = e.emp_no
+join departments as d
+on d.dept_no = dm.dept_no
+WHERE dm.to_date = '9999-01-01'
+order by d.dept_name asc;
+
+select d.dept_name as Department_Name, concat(e.first_name, ' ', e.last_name ) as Department_Manager
+from employees as e
+         join dept_manager as dm
+              on dm.emp_no = e.emp_no
+         join departments as d
+              on d.dept_no = dm.dept_no
+WHERE dm.to_date = '9999-01-01'
+and e.gender = 'f';
+
+select t.title as Title, count(t.title) as Total
+from titles as t
+    join dept_emp as de
+      on de.emp_no = t.emp_no
+    join departments as d
+      on  d.dept_no=de.dept_no
+WHERE de.to_date = '9999-01-01'
+  and t.to_date = '9999-01-01'
+and de.dept_no = 'd009'
+group by t.title;
+
+select d.dept_name as Department_Name, concat(e.first_name, ' ', e.last_name ) as Department_Manager, s.salary as Salary
+from employees as e
+         join dept_manager as dm
+              on dm.emp_no = e.emp_no
+         join departments as d
+              on d.dept_no = dm.dept_no
+        join dept_emp as de
+            on dm.emp_no = de.emp_no
+        join salaries as s
+            on de.emp_no = s.emp_no
+WHERE dm.to_date = '9999-01-01'
+  and s.to_date = '9999-01-01'
+order by d.dept_name asc;
